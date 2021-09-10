8931439048567a5270d80078f325ab1227332b84
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-10 18:59:30 +0000

    Finished creating the questions, working on code.

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 #Question 1
-print("Question 1: What is the sum of 2 + 2 ?", '\n', "A) 2", '\n', "B) 4", '\n', "C) 6", '\n', "D) 8")
+print("Question 1: What is the tallest structure in the world? Type the letter of the option choice to answer.", '\n', "A) Tokyo Skytree", '\n', "B) Burj Khalifa", '\n', "C) Canton Tower", '\n', "D) Abraj Al Bait")
+answer = input()
+if answer == B:
+    print("Correct!")
+else:
+    print("Incorrect.")
 
-print("Question  2:  Is thie following statement true or false?
+
+#Question 2
+print("Question  2:  Is thie following statement true or false? The sun is a star.", '\n',  "True", '\n', "False")
+
+
+
+#Question 3
+print("Question 3: Fill in the blank to complete Mr. Adam's infamous quote!", '\n' "Eary is on time, on time is late, and ____ is never acceptable!")
+
+
+
+#Question 4
+print("Question 4: What is the sum of 2+2 ?")
+
+
+
+#Question 5
+print("In what year did the first cat go to space?")
+
