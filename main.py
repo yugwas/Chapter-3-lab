a88b2db6f9620bb9b3cc6c99877edd40d15934bd
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-10 20:28:39 +0000

    Finished making the quiz. Added the score function and gives the user the percentage.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+score = 0
+number_of_questions = 5
+
 # Question 1
 print("Question 1: What is the tallest structure in the world? Type the letter of the option choice to answer.", '\n', "A) Tokyo Skytree", '\n', "B) Burj Khalifa", '\n', "C) Canton Tower", '\n', "D) Abraj Al Bait")
 answer = input().lower()
 if answer == "b":
     print("Correct!", '\n')
+    score += 1
 else:
     print("Incorrect.", '\n')
 
@@ -11,6 +15,7 @@ print("Question  2:  Is thie following statement true or false? The sun is a sta
 answer = input().lower()
 if answer == "true":
     print("Correct!", '\n')
+    score += 1
 else:
     print("Incorrect.", '\n')
 
@@ -19,6 +24,7 @@ print("Question 3: Fill in the blank to complete Mr. Adam's infamous quote!", '\
 answer = input().lower()
 if answer == "late":
     print("Correct!", '\n')
+    score += 1
 else:
     print("Incorrect.", '\n')
 
@@ -29,6 +35,7 @@ if answer == "4":
     print("Correct!", '\n')
 elif answer == "four":
     print("Correct!", '\n')
+    score += 1
 else:
     print("Incorrect.", '\n')
 
@@ -37,7 +44,8 @@ print("In what year did the first cat go to space?")
 answer = input().lower()
 if answer == "1963":
     print("Correct!", '\n')
+    score +=1
 else:
     print("Incorrect.", '\n')
 
-
+print("Your score is", score, "out of", number_of_questions, "which is", str(score/number_of_questions * 100) + "%")
