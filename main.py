789c8bb004ecb14354316b309877fb23c3eba4a6
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-10 19:10:40 +0000

    Started working on the answers for the questions and telling if the user got the question right or not.

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-#Question 1
+# Question 1
 print("Question 1: What is the tallest structure in the world? Type the letter of the option choice to answer.", '\n', "A) Tokyo Skytree", '\n', "B) Burj Khalifa", '\n', "C) Canton Tower", '\n', "D) Abraj Al Bait")
 answer = input()
-if answer == B:
-    print("Correct!")
+if answer == "B":
+    print("Correct!", '\n')
 else:
-    print("Incorrect.")
+    print("Incorrect.", '\n')
 
+# I should fix: Tell Python to accept lowercase and uppercase B as the correct answer.
 
-#Question 2
+# Question 2
 print("Question  2:  Is thie following statement true or false? The sun is a star.", '\n',  "True", '\n', "False")
 
 
 
-#Question 3
+# Question 3
 print("Question 3: Fill in the blank to complete Mr. Adam's infamous quote!", '\n' "Eary is on time, on time is late, and ____ is never acceptable!")
 
 
 
-#Question 4
+# Question 4
 print("Question 4: What is the sum of 2+2 ?")
 
 
 
-#Question 5
+# Question 5
 print("In what year did the first cat go to space?")
 
