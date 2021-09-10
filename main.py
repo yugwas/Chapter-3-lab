83b78c8d846a017e20e765d4dc25904523572955
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-10 20:01:27 +0000

    Did the code to tell the user if they got a question right or wrong.

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 # Question 1
 print("Question 1: What is the tallest structure in the world? Type the letter of the option choice to answer.", '\n', "A) Tokyo Skytree", '\n', "B) Burj Khalifa", '\n', "C) Canton Tower", '\n', "D) Abraj Al Bait")
-answer = input()
-if answer == "B":
+answer = input().lower()
+if answer == "b":
     print("Correct!", '\n')
 else:
     print("Incorrect.", '\n')
 
-# I should fix: Tell Python to accept lowercase and uppercase B as the correct answer.
-
 # Question 2
 print("Question  2:  Is thie following statement true or false? The sun is a star.", '\n',  "True", '\n', "False")
-
-
+answer = input().lower()
+if answer == "true":
+    print("Correct!", '\n')
+else:
+    print("Incorrect.", '\n')
 
 # Question 3
 print("Question 3: Fill in the blank to complete Mr. Adam's infamous quote!", '\n' "Eary is on time, on time is late, and ____ is never acceptable!")
-
-
+answer = input().lower()
+if answer == "late":
+    print("Correct!", '\n')
+else:
+    print("Incorrect.", '\n')
 
 # Question 4
 print("Question 4: What is the sum of 2+2 ?")
-
-
+answer = input().lower()
+if answer == "4":
+    print("Correct!", '\n')
+elif answer == "four":
+    print("Correct!", '\n')
+else:
+    print("Incorrect.", '\n')
 
 # Question 5
 print("In what year did the first cat go to space?")
+answer = input().lower()
+if answer == "1963":
+    print("Correct!", '\n')
+else:
+    print("Incorrect.", '\n')
+
 
