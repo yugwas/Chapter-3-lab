bb58bbe93c63863986c6fb9f93ee3770efa0ac4b
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-10 17:28:50 +0000

    adding question 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #Question 1
 print("Question 1: What is the sum of 2 + 2 ?", '\n', "A) 2", '\n', "B) 4", '\n', "C) 6", '\n', "D) 8")
 
+print("Question  2:  Is thie following statement true or false?
